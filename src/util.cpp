234dbefd4dbca22f09a11540b149851f8485799d
post@albertvanandel.nl <you@example.com>
2024-03-28 17:38:58 +0100

    new try for portable time formatting

--- src/util.cpp
+++ src/util.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <cstring>
 #include <chrono>
+#include <format>
 #include <filesystem>
 #include <sstream>
 #include <iomanip>
@@ -37,6 +38,34 @@ bool WebServUtil::_compareDirectoryContents(const std::filesystem::path& one, co
     return oneIsDir > twoIsDir;
 }
 
+std::string fileTimeToString(const std::filesystem::file_time_type& fileTime) {
+    using namespace std::chrono;
+
+    // Extracting the duration since the epoch
+    auto duration_since_epoch = fileTime.time_since_epoch();
+
+    // Converting to seconds
+    auto seconds_since_epoch = duration_cast<seconds>(duration_since_epoch);
+
+    // Constructing time_point from seconds
+    system_clock::time_point tp{seconds_since_epoch};
+
+    // Converting time_point to time_t
+    auto time_t_point = system_clock::to_time_t(tp);
+
+    // Converting to tm structure
+    std::tm tm;
+#ifdef _WIN32
+    localtime_s(&tm, &time_t_point);
+#else
+    localtime_r(&time_t_point, &tm);
+#endif
+
+    // Formatting time
+    std::stringstream ss;
+    ss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+    return ss.str();
+}
 std::string WebServUtil::directoryIndexList(const std::string& path, const std::string& request_adress) {
     std::vector<std::filesystem::path> entries;
     std::string title, filename;
@@ -57,11 +86,16 @@ std::string WebServUtil::directoryIndexList(const std::string& path, const std::
 		if (filename.length() > DEFAULT_MAX_FILENAME_DISPLAY)
 			filename = filename.substr(0, DEFAULT_MAX_FILENAME_DISPLAY) + "..>";
 		index_listing << "<a href=\"" << it.filename().string() << "\">" << std::setw(DEFAULT_MAX_FILENAME_DISPLAY) << std::left << filename + "</a>";
-		auto last_write_time = std::filesystem::last_write_time(it);	
-std::time_t cformat_time = decltype(last_write_time)::clock::to_time_t(last_write_time);
-    char* formatted_time = std::ctime(&cformat_time);
-    formatted_time[std::strlen(formatted_time) - 1] = '\0';
-		index_listing << "\t" << formatted_time;
+	
+	auto last_write_time = std::filesystem::last_write_time(it);	
+	// auto system_time = std::chrono::clock_cast<std::chrono::system_clock>(last_write_time);
+	// auto cformat_time = std::chrono::system_clock::to_time_t(system_time);
+
+	//
+	// std::time_t cformat_time = decltype(last_write_time)::clock::to_time_t(last_write_time);
+    // char* formatted_time = std::ctime(&cformat_time);
+    // formatted_time[std::strlen(formatted_time) - 1] = '\0';
+		index_listing << "\t" << fileTimeToString(last_write_time);
 		if (!std::filesystem::is_directory(it))
 			index_listing << "\t" << std::filesystem::file_size(it);
 		else
